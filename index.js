3bd51c5509ff9064d57113684ac0db1e9d1ab937
Jose Fletes <joseangelfletes@gmail.com>
2022-01-31 12:44:32 -0800

    commit tree cf85b749e0af8374c0fc64f0121a5018dca631fd

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) +1;
+
+let num3 = 36;
+let num4 = 4;
+let mod = num4 % num3;
+
+const max = Math.max(1, 2, 3, 20);
